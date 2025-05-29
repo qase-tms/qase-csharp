16f5ce29350825ccd7a33a786d55e5066ff1ee44
Dmitrii Gridnev <dgridnev@qase.io>
2025-05-29 17:02:21 +0200

    feat: add xUnit example tests and README documentation for demonstration of test framework usage

---
+++ examples/xUnitExamples/SimpleTests.cs
@@ -0,0 +1,50 @@
+﻿using Qase.Csharp.Commons.Attributes;
+
+namespace xUnitExamples;
+
+[Fields("preconditions", "Some data")]
+[Suites("Main suite", "Sub suite")]
+public class SimpleTests
+{
+    [Fact]
+    [Title("Test with title")]
+    public void TestWithTitle()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    [QaseIds(1)]
+    public void TestWithSingleId()
+    {
+    }
+
+    [Fact]
+    [QaseIds(2, 3)]
+    public void TestWithMultipleIds()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+
+    [Fact]
+    [Fields("description", "Test description")]
+    [Fields("severity", "critical")]
+    public void TestWithFields()
+    {
+        Assert.NotEqual(5, 2 + 2);
+    }
+
+    [Fact]
+    [Ignore]
+    public void TestWithIgnore()
+    {
+        Assert.True(true);
+    }
+
+
+    [Fact]
+    [Suites("Unique suite")]
+    public void TestWithUniqueSuite()
+    {
+    }
+}
